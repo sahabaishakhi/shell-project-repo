9019cc7aff5eec06b8044d0f5f5a13f3836e7840
VikramShawFL <vikram@front.live>
2019-01-17 00:06:40 +0530

    3rd branch

--- src/shell_project/core.clj
+++ src/shell_project/core.clj
@@ -2,5 +2,7 @@
 
 (defn foo
   "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  [x y z]
+  (println x "Hello, World!")
+  (println y "This is the 3rd branch")
+  (println z "Have not added a nwfile but edited the existing file"))
