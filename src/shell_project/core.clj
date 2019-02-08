3b6cc2e5b19f170cd7dc580f3b9e86bf2576986b
VikramShawFL <vikram@front.live>
2019-02-08 14:01:08 +0530

    check

--- src/shell_project/core.clj
+++ src/shell_project/core.clj
@@ -4,5 +4,5 @@
   "I don't do a whole lot."
   [x y z]
   (println x "Hello, World!")
-  (println y "This is the 3rd branch")
+  (println y "This is to check anything")
   (println z "Have not added a nwfile but edited the existing file"))
