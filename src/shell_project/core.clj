61ded0ac5a7eefe4a1bef027b1c95ec0dd8534b4
sahabaishakhi <baishakhi@front.live>
2019-02-11 20:52:38 +0530

    com msg

--- src/shell_project/core.clj
+++ src/shell_project/core.clj
@@ -2,7 +2,8 @@
 
 (defn foo
   "I don't do a whole lot."
-  [x y z]
+  [x y z w]
   (println x "Hello, World!")
   (println y "This is to check anything")
-  (println z "Have not added a nwfile but edited the existing file"))
+  (println z "Have not added a nwfile but edited the existing file")
+  (println w "testing out pull requests"))
